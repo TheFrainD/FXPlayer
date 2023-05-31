a50a45c7069dca13b40259b89f0ab1051aae0785
Dmytro Zykov <dmytri.zykov@gmail.com>
2023-05-31 21:00:25 +0300

    Implement video loading

--- app/src/main/java/com/frain/fxplayer/MainActivity.kt
+++ app/src/main/java/com/frain/fxplayer/MainActivity.kt
@@ -1,14 +1,40 @@
 package com.frain.fxplayer
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.frain.fxplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            Toast.makeText(this, "Loaded", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.loadVideoButton.setOnClickListener {
+            openVideoSelector()
+        }
     }
+
+    private fun openVideoSelector() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "video/*"
+        }
+
+        getResult.launch(intent)
+    }
+
 }
