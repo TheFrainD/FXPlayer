6b0e8132ee4ec6bcc1ae6779f467a7d3a5500a29
Dmytro Zykov <dmytri.zykov@gmail.com>
2023-05-31 22:24:48 +0300

    Implement video and audio playing via ExoPlayer

--- app/src/main/java/com/frain/fxplayer/MainActivity.kt
+++ app/src/main/java/com/frain/fxplayer/MainActivity.kt
@@ -2,36 +2,85 @@ package com.frain.fxplayer
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.frain.fxplayer.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var binding: ActivityMainBinding
+    private var exoPlayer: ExoPlayer? = null
+    private var playWhenReady = true
+    private var playbackPosition = 0L
 
     private val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == Activity.RESULT_OK) {
-            Toast.makeText(this, "Loaded", Toast.LENGTH_SHORT).show()
+            it.data?.data?.also { uri ->
+                preparePlayer(uri)
+            }
+
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setView()
 
-        binding.loadVideoButton.setOnClickListener {
+        binding.loadMediaButton.setOnClickListener {
             openVideoSelector()
         }
     }
 
+    private fun setView() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun preparePlayer(videoUri: Uri) {
+        exoPlayer = ExoPlayer.Builder(this).build()
+        binding.playerView.player = exoPlayer
+        exoPlayer?.apply {
+            setMediaItem(MediaItem.fromUri(videoUri))
+            seekTo(playbackPosition)
+            playWhenReady = playWhenReady
+            prepare()
+        }
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.let { player ->
+            playbackPosition = player.currentPosition
+            playWhenReady = player.playWhenReady
+            player.release()
+            exoPlayer = null
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
+    }
+
     private fun openVideoSelector() {
         val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
-            type = "video/*"
+            val mimeTypes = arrayOf("video/*", "audio/*")
+            type = "*/*"
+            putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
         }
 
         getResult.launch(intent)
