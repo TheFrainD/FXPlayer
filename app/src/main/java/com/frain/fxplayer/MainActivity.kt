e559b0051da9155460284e99e62d2fac599af0ee
Dmytro Zykov <dmytri.zykov@gmail.com>
2023-05-31 19:54:22 +0300

    Move to view binding

--- app/src/main/java/com/frain/fxplayer/MainActivity.kt
+++ app/src/main/java/com/frain/fxplayer/MainActivity.kt
@@ -2,10 +2,13 @@ package com.frain.fxplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.frain.fxplayer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
